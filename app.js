5d53af4b1318949d0d35a5ef9270a6b0bab55acb
Joel <joeljacobs95@gmail.com>
2020-08-21 21:48:14 -0400

    fixed to change colors again

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ function rand() {
 }
 function randomRGB() {
 	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
 	const b = Math.floor(Math.random() * 256);
-	return `rgb(${r},${b})`;
+	return `rgb(${r},${g},${b})`;
 }
 
 setInterval(function() {
